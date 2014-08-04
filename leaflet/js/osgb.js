c988b942915b2b15a3e33a155c7010ff2069b761
matthew-shaw <matthew.shaw@hotmail.co.uk>
2014-08-04 17:04:17 +0100

    update osgb crs

--- leaflet/js/osgb.js
+++ leaflet/js/osgb.js
@@ -1,6 +1,15 @@
+var osgb = new L.Proj.CRS('EPSG:27700',
+  '+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000' +
+  '+ellps=airy +datum=OSGB36 +units=m +no_defs',
+  {
+    resolutions: [2500, 1000, 500, 200, 100, 50, 25, 10, 5, 2.5],
+    bounds: L.bounds([1300000,0],[700000,0])
+  }
+);
+
 /* L.Map with OS options */
 var map = new L.Map('map', {
-  crs: L.OSOpenSpace.getCRS(),
+  crs: osgb,
   continuousWorld: true,
   worldCopyJump: false,
   minZoom: 2,
