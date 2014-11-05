f8eaba12317263a083059acec1cff53a884fad8a
matthew-shaw <matthew.shaw@digital.landregistry.gov.uk>
2014-11-05 15:54:09 +0000

    trying geolocation

--- leaflet/js/offices.js
+++ leaflet/js/offices.js
@@ -45,3 +45,23 @@ var overlays = {
 };
 
 L.control.layers(baseLayers, overlays).addTo(map);
+
+L.control.scale().addTo(map);
+
+function onLocationFound(e) {
+  var radius = e.accuracy / 2;
+
+  L.marker(e.latlng).addTo(map)
+    .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+  L.circle(e.latlng, radius).addTo(map);
+}
+
+function onLocationError(e) {
+  alert(e.message);
+}
+
+map.on('locationfound', onLocationFound);
+map.on('locationerror', onLocationError);
+
+map.locate({setView: true, maxZoom: 16});
